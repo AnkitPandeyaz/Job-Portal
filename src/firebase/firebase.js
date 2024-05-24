c46c1bc475f69396d8ec830314f038c11d261203
Ankit Pandey <115497579+AnkitPandeyaz@users.noreply.github.com>
2024-05-24 10:08:12 +0530

    Update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,6 @@
 // import 'firebase/auth';
 // import 'firebase/firestore'; // If you're using Firestore
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAgHy03mUtfV7DBuo8pg2wrtYo9ZXWkRJI",
-//     authDomain: "job-portal-13fdf.firebaseapp.com",
-//     projectId: "job-portal-13fdf",
-//     storageBucket: "job-portal-13fdf.appspot.com",
-//     messagingSenderId: "130777784436",
-//     appId: "1:130777784436:web:cc151aa83672fd5781f121"
-//   };
 
 // // Initialize Firebase
 // const app = firebase.initializeApp(firebaseConfig);
@@ -21,14 +13,6 @@
 // import 'firebase/auth';
 // import 'firebase/firestore';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAgHy03mUtfV7DBuo8pg2wrtYo9ZXWkRJI",
-//     authDomain: "job-portal-13fdf.firebaseapp.com",
-//     projectId: "job-portal-13fdf",
-//     storageBucket: "job-portal-13fdf.appspot.com",
-//     messagingSenderId: "130777784436",
-//     appId: "1:130777784436:web:cc151aa83672fd5781f121"
-//   };
 
 // // Check if Firebase app is already initialized
 // if (!firebase.apps.length) {
