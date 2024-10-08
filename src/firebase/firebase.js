953fc76ab9f6a4508a10daa7288c32e0772cb5f3
Ankit Pandey <115497579+AnkitPandeyaz@users.noreply.github.com>
2024-10-09 00:12:24 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
- apiKey: "AIzaSyAOQ0D6kuk6OanryvzODhIiA3VLkIX78ms",
-  authDomain: "fit-it-782e9.firebaseapp.com",
-  databaseURL: "https://fit-it-782e9-default-rtdb.firebaseio.com",
-  projectId: "fit-it-782e9",
-  storageBucket: "fit-it-782e9.appspot.com",
-  messagingSenderId: "846087500602",
-  appId: "1:846087500602:web:af177cb0995ba9d4031603"
+
 };
 
 const app = initializeApp(firebaseConfig);
