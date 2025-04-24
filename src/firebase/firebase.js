0e9eddc63336b9db22c282b74f846113b9b5c04b
AnkitPandeyaz <pandeyankit713@gmail.com>
2025-04-24 23:36:30 +0530

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
- 
+    apiKey: "AIzaSyDBis3Soy_Srz9saiEa408R6ncOewJjTxY",
+    authDomain: "online-pizza-system.firebaseapp.com",
+    projectId: "online-pizza-system",
+    storageBucket: "online-pizza-system.firebasestorage.app",
+    messagingSenderId: "386507580479",
+    appId: "1:386507580479:web:ce03ea9e3f9ca61d64c48a"
 };
 
 const app = initializeApp(firebaseConfig);
